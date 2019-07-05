813063a1e0d244a86f58a6ef3e434be76b70bdf2
Talha Çağatay IŞIK <talhacagatayisik@gmail.com>
2019-07-05 22:09:38 +0300

    game controller created

---
+++ Okey-Game/Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace TÇI
+{
+    public class GameController : MonoBehaviour
+    {
+        public static event Action GameControllerInitialized;
+
+        private void Awake()
+        {
+            SubscribeEvents();
+
+            if(GameControllerInitialized != null)
+            {
+                Debug.Log("GameController Initialized");
+                GameControllerInitialized.Invoke();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            UnSubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+
+        }
+
+        private void UnSubscribeEvents()
+        {
+
+        }
+    }
+}
