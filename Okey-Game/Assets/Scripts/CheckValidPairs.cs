93a2691d7ee6589cca1afee7e69265900d8539cb
Talha Çağatay IŞIK <talhacagatayisik@gmail.com>
2019-07-06 03:17:59 +0300

    this class will be responsible of checking all hands and finding best one

---
+++ Okey-Game/Assets/Scripts/CheckValidPairs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TÇI
+{
+    public static class FindBestHand
+    {
+        public static List<TileModel> GetBestHand()
+        {
+            return null;
+        }
+    }
+}
