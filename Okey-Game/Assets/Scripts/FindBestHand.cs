ff1a69eacc1e296471d744d1a3b384980f20e8ce
Talha Çağatay IŞIK <talhacagatayisik@gmail.com>
2019-07-16 15:50:44 +0300

    algorithm improved to find best hand

--- Okey-Game/Assets/Scripts/FindBestHand.cs
+++ Okey-Game/Assets/Scripts/FindBestHand.cs
@@ -1,163 +1,214 @@
 ﻿using System.Collections.Generic;
+using System;
 using System.Linq;
 using UnityEngine;
 
 namespace TÇI
 {
-    /// <summary>
-    /// this class is for checking series and groups in players hand and finding the best one
-    /// I haven't got enough time to make this algorithm better but this will work.
-    /// 
-    /// /// What does the algorithm do :
-    /// 1-) Firstly it is ordering tiles by number and trying to find the "groups"
-    /// 2-) After finding groups it is ordering tiles by colors and numbers and trying to find "runs"
-    /// 
-    /// What also needs to be done :
-    /// 1-) Algorith should check every combination of sets with "runs" and "groups" in order to find which set is leaving out least amount of point
-    /// 2-) Also a remainder that "2345" is almost twice better from "1234" because both 1 and 6 can help but for the second one only 5 will help    
-    /// 
-    /// So, the algorithm is working correctly by not in an effective way but i will make it more efficient shortly...
-    /// I didn't add a check for jokers yet
-    /// </summary>
-    public class FindBestHand
+    internal struct TileSet
     {
-        public static Player GetBestHand(Player[] players)
+        public readonly List<TileModel> tiles;
+        public readonly int jokersCount;
+
+        public readonly int cost;
+
+        public TileSet(List<TileModel> tiles, int jokersCount)
         {
-            int score = int.MaxValue;
-            Player bestPlayer = null;
-
-            for (int i = 0; i < players.Length; i++)
-            {
-                if (GetScore(players[i].Tiles) < score)
-                {
-                    score = GetScore(players[i].Tiles);
-                    bestPlayer = players[i];
-                }
-            }
-
-            return bestPlayer; // this will return the player who has the best hand
+            this.tiles = tiles;
+            this.jokersCount = jokersCount;
+            this.cost = tiles.Sum(tile => tile.Number) + jokersCount * 30;
         }
 
-        public static int GetScore(List<TileModel> tileModels)
-        {           
-            List<TileModel> meld = new List<TileModel>(); // creating an empty list to hold a meld
-            List<TileModel> allMelds = new List<TileModel>();//([1,2,3],[5,6,7],[9,10,11]) this list is responsible of holding all melds
-            //List<List<TileModel>> also an option or maybe a dictionary could be use but since the indexes doesnt matter dictionary will be pointless
+    }
 
-            //we are ordering the player's hand with tiles numbers...1,2,3 etc
-            List<TileModel> sortedTileModel = tileModels.OrderBy(o => o.Number).ToList();
+    internal class JokerRunData // contained in list and modified from there, so can't be struct
+    {
+        public readonly int runStart;
+        public int runEnd;
+        public List<int> jokersUsed;
+
+        public JokerRunData(int runStart)
+        {
+            this.runStart = runStart;
+            this.runEnd = -1;
+            this.jokersUsed = new List<int>(2);
+        }
+    }
+
+    public static class FindBestHand
+    {
+        public static Player GetBestHand(Player[] players) =>
+        players.Select(pl => Tuple.Create(pl, GetScore(pl.Tiles)))
+        .Aggregate((t1, t2) => t1.Item2 < t2.Item2 ? t1 : t2).Item1;
+
+        public static int GetScore(List<TileModel> playerTiles)
+        {
+            var allSets = new List<TileSet>();
+
+            var jokerCount = playerTiles.Count(tile => tile.IsJoker);
 
             #region Finding "GROUPS"
-            var firstTile = sortedTileModel[0]; // getting the first tile
-            meld.Add(firstTile);//adding first tile in meld list
 
-            for (int x = 1; x < sortedTileModel.Count; x++)
             {
-                for (int y = 0; y < meld.Count; y++)
-                {
-                    if(sortedTileModel[x].Color == meld[y].Color || sortedTileModel[x].Number != meld[y].Number)//comparing new tile with every tile in the meld to check if we should add it or remove it
-                    {
-                        if(meld.Count < 3)
-                        {
-                            meld.Clear();
-                            meld.Add(sortedTileModel[x]);
-                            break;
-                        }
-                        else // if our meld's count is >= 3 then we dont have to broke that meld but start a new one and add them to allMelds list
-                        {
-                            for (int i = 0; i < meld.Count; i++)
-                            {
-                                allMelds.Add(meld[i]);
-                            }
+                var groups = new HashSet<TileColor>[13]; // can't be 7red + 7red + 7black, need 3 differenct colors, so HashSet
+                for (int i = 1; i < 13; i++) groups[i] = new HashSet<TileColor>();
 
-                            meld.Clear();
-                        }
-                    }
-                }
+                foreach (var tile in playerTiles)
+                    if (!tile.IsJoker)
+                        groups[tile.Number - 1].Add(tile.Color);
 
-                if(sortedTileModel[x].Color != firstTile.Color && sortedTileModel[x].Number == firstTile.Number)
-                {
-                    meld.Add(sortedTileModel[x]);
-                }
+                for (int i = 12; i >= 0; i--) // for in reverse to sort less leter. this way groups of 13 would be before groups of 1, and we need this to spend 13's first
+                    if (groups[i].Count >= 3)
+                        allSets.Add(new TileSet(
+                            groups[i].Select(color => new TileModel(i + 1, color, false)).ToList(),
+                            0
+                        ));
+                    else if (groups[i].Count >= 1) // can 2 jokers be used in 1 run/group? if not - we need "==2" here
+                        allSets.Add(new TileSet(
+                            groups[i].Select(color => new TileModel(i + 1, color, false)).ToList(),
+                            3 - groups[i].Count
+                        ));
 
-                firstTile = sortedTileModel[x];
             }
-            #endregion
 
-            // removing the groups from all tiles
-            for (int i = 0; i < meld.Count; i++)
-            {
-                allMelds.Remove(meld[i]);
-            }
-            meld.Clear();
+            #endregion Finding "GROUPS"
 
             #region Finding "RUNS"
-            //we are ordering the player's hand with tiles Colors and Numbers...Yellow0,Yellow1,Red0,Red1 etc
-            sortedTileModel = tileModels.OrderBy(o => o.Color).ThenBy(o => o.Number).ToList();            
 
-            firstTile = sortedTileModel[0]; // getting the first tile
-            meld.Add(firstTile);//adding first in the meld list
-
-            for (int x = 1; x < sortedTileModel.Count; x++)
             {
-                for (int y = 0; y < meld.Count; y++)
+                var coloredTiles = new Dictionary<TileColor, int[]>(4); // Value's are array of counts of tiles of said number and corresponding Key-color
+                foreach (TileColor color in Enum.GetValues(typeof(TileColor))) coloredTiles[color] = new int[13];
+
+                foreach (var tile in playerTiles)
+                    if (!tile.IsJoker)
+                        coloredTiles[tile.Color][tile.Number - 1] += 1;
+
+                foreach (var color in coloredTiles.Keys)
                 {
-                    if(sortedTileModel[x].Color != meld[y].Color || (sortedTileModel[x].Number -1 != firstTile.Number && sortedTileModel[x].Number != firstTile.Number))
+                    var currTiles = coloredTiles[color];
+
+                    var mainRunStart = -1; // -1 if no run is being enumerated and [i] if tile with .Number==i+1 is last in currently enumerated run
+                    var jokerRuns = new List<JokerRunData>();
+
+                    for (int i = 12; i >= 0; i--)
                     {
-                        if (meld.Count < 3)
+                        if (currTiles[i] != 0)
                         {
-                            meld.Clear();
-                            meld.Add(sortedTileModel[x]);
-                            break;
+                            if (mainRunStart == -1) mainRunStart = i;
+
+                            if (currTiles[i] == 2) // if we have 1,2,3,3,4,5 - we can make 123+345 instead of 12345, where last would clearly cost less
+                            {
+
+                                if (i < mainRunStart - 1)
+                                {
+
+                                    allSets.Add(new TileSet(
+                                        Enumerable.Range(i, mainRunStart - i + 1).Select(num => new TileModel(num + 1, color, false)).ToList(),
+                                        0
+                                    ));
+
+                                    mainRunStart = i;
+                                }
+
+                                for (var jri = 0; i < jokerRuns.Count; i++)
+                                    if (jokerRuns[jri].runEnd == -1)
+                                        if (i < jokerRuns[jri].runStart - 1)
+                                            jokerRuns[jri].runEnd = i;
+
+                            }
+
                         }
                         else
                         {
-                            for (int i = 0; i < meld.Count; i++)
+
+                            if (mainRunStart != -1)
                             {
-                                allMelds.Add(meld[i]);
+                                {
+                                    var jokerRun = new JokerRunData(mainRunStart);
+                                    jokerRun.jokersUsed.Add(i);
+                                    jokerRuns.Add(jokerRun);
+                                }
+
+                                if (i < mainRunStart - 1)
+                                    allSets.Add(new TileSet(
+                                        Enumerable.Range(i, mainRunStart - i + 1).Select(num => new TileModel(num + 1, color, false)).ToList(),
+                                        0
+                                    ));
+
+                                mainRunStart = -1;
                             }
 
-                            meld.Clear();
-                            break;
+                            for (var jri = 0; i < jokerRuns.Count; i++)
+                                if (jokerRuns[jri].runEnd == -1)
+                                {
+                                    if (jokerRuns[jri].jokersUsed.Count < 2)
+                                        jokerRuns[jri].jokersUsed.Add(i);
+                                    else
+                                        jokerRuns[jri].runEnd = i + 1;
+                                }
+
                         }
-                    }                                                            
+
+                        if (i < mainRunStart - 1)
+                            allSets.Add(new TileSet(
+                                Enumerable.Range(0, mainRunStart + 1).Select(num => new TileModel(num + 1, color, false)).ToList(),
+                                0
+                            ));
+                    }                        
+
+                    foreach (var jokerRun in jokerRuns)
+                    {
+                        allSets.Add(new TileSet(
+                            Enumerable.Range(jokerRun.runEnd, jokerRun.runStart - jokerRun.runEnd + 1)
+                            .Where(num => !jokerRun.jokersUsed.Contains(num))
+                            .Select(num => new TileModel(num + 1, color, false)).ToList(),
+                            jokerRun.jokersUsed.Count
+                        ));
+
+                        if (jokerRun.jokersUsed.Count == 2)
+                        {
+                            allSets.Add(new TileSet(
+                                Enumerable.Range(jokerRun.jokersUsed[1], jokerRun.runStart - jokerRun.jokersUsed[1] + 1)
+                                .Where(num => jokerRun.jokersUsed[0] != num)
+                                .Select(num => new TileModel(num + 1, color, false)).ToList(),
+                                1
+                            ));
+                        }
+
+                    }
+
                 }
 
-                if (sortedTileModel[x].Color == firstTile.Color && sortedTileModel[x].Number - 1 == firstTile.Number)
-                {
-                    meld.Add(sortedTileModel[x]);
-                }
-
-                if (sortedTileModel[x].Number == firstTile.Number)
-                {
-                    continue;
-                }
-
-                firstTile = sortedTileModel[x];
             }
-            #endregion
 
-            List<TileModel> tempTileList = new List<TileModel>(); // this is temporary and will be cleaned up later
+            #endregion Finding "RUNS"
 
-            for (int i = 0; i < tileModels.Count; i++)
+
+
             {
-                tempTileList.Add(tileModels[i]);
+                var n_allSets = new List<TileSet>(allSets.Count);
+                n_allSets.AddRange(allSets.OrderByDescending(ts => ts.cost));
+                allSets = n_allSets;
             }
 
-            //removing melds from initial hand
-            for (int i = 0; i < allMelds.Count; i++)
+            var n_playerTiles = playerTiles.ToList();
+            var usedTiles = new HashSet<TileModel>();
+
+            while (allSets.Any())
             {
-                tempTileList.Remove(allMelds[i]);
+                var curr = allSets[0];
+                foreach (var tile in curr.tiles) usedTiles.Add(tile);
+                jokerCount -= curr.jokersCount;
+
+                //n_playerTiles.RemoveAll(curr.tiles.Contains); //TODO would only work if TileModel would be struct. otherwise we need:
+                n_playerTiles.RemoveAll(tile => curr.tiles.Any(c_tile => (tile.Color == c_tile.Color) && (tile.Number == c_tile.Number)));
+
+                //TODO and same again:
+                //allSets.RemoveAll(ts=> (ts.jokersCount>jokerCount) || ts.tiles.Any(curr.tiles.Contains) );
+                allSets.RemoveAll(ts => (ts.jokersCount > jokerCount) || ts.tiles.Any(tile => curr.tiles.Any(c_tile => (tile.Color == c_tile.Color) && (tile.Number == c_tile.Number))));
             }
 
-            //now we can calculate score by adding all tiles number those are out of sets--meaning out of any groups/runs
-            int score = 0;
-            for (int i = 0; i < tempTileList.Count; i++)
-            {
-                score += tempTileList[i].Number;
-            }
-
-            return score;            
+            return n_playerTiles.Sum(tile => tile.Number) + jokerCount * 30;
         }
     }
 }
